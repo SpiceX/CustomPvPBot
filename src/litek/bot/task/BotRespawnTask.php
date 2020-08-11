1a4b414db8018c109e9c4005dfe2c9aaabc8ecee
LiTEKPE <pocketminesmash@gmail.com>
2020-08-11 19:23:06 +0000

    build 03

--- src/litek/bot/task/BotRespawnTask.php
+++ src/litek/bot/task/BotRespawnTask.php
@@ -12,37 +12,37 @@ use pocketmine\scheduler\Task;
 class BotRespawnTask extends Task
 {
 
-	/** @var Bot */
-	private $bot;
+    /** @var Bot */
+    private $bot;
 
-	/** @var int */
-	private $seconds;
+    /** @var int */
+    private $seconds;
 
-	/**
-	 * BotRespawnTask constructor.
-	 * @param Bot $bot
-	 */
-	public function __construct(Bot $bot)
-	{
-		$this->bot = $bot;
-		$this->seconds = $bot->getRespawnTime();
-	}
+    /**
+     * BotRespawnTask constructor.
+     * @param Bot $bot
+     */
+    public function __construct(Bot $bot)
+    {
+        $this->bot = $bot;
+        $this->seconds = $bot->getRespawnTime();
+    }
 
-	public function onRun(int $currentTick): void
-	{
-		if ($this->seconds === null || $this->bot === null){
-			$this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-			return;
-		}
-		if ($this->seconds <= 0) {
-			$template = $this->getPlugin()->getTemplateManager()->getTemplate($this->bot->name);
-			if ($template === null){
-				$this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-				return;
-			}
-			try {
-			    $abusedPlayer= $this->getAbusingPlayer();
-			    if ($abusedPlayer !== null){
+    public function onRun(int $currentTick): void
+    {
+        if ($this->seconds === null || $this->bot === null) {
+            $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+            return;
+        }
+        if ($this->seconds === 0) {
+            $template = $this->getPlugin()->getTemplateManager()->getTemplate($this->bot->name);
+            if ($template === null) {
+                $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+                return;
+            }
+            try {
+                $abusedPlayer = $this->getAbusingPlayer();
+                if ($abusedPlayer !== null) {
                     $bot = $this->getPlugin()->getEntityManager()->prepareBot($abusedPlayer, $this->bot->getDefaultPosition());
                     $command = $template->getCommand();
                     $bot->setName($template->getName());
@@ -53,44 +53,41 @@ class BotRespawnTask extends Task
                     $bot->setSkin($template->getSkin());
                     $bot->setRespawnTime($template->getRespawnTime());
                     $bot->sendSkin();
-                    if (!$bot->isFlaggedForDespawn()){
-                        $bot->spawnToAll();
-                        $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-                        return;
-                    }
+                    $bot->spawnToAll();
+                    $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+                    return;
                 }
-			} catch (InvalidSkinException $exception) {
-				$this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-				return;
-			} catch (Exception $exception){
-				$this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-				return;
-			}
-			$this->bot->spawnToAll();
-			$this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
-			return;
-		}
-		$this->seconds--;
-	}
+            } catch (InvalidSkinException $exception) {
+                $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+                return;
+            } catch (Exception $exception) {
+                $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+                return;
+            }
+            $this->getPlugin()->getScheduler()->cancelTask($this->getTaskId());
+            return;
+        }
+        $this->seconds--;
+    }
 
-	/**
-	 * @return CustomPvPBot
-	 */
-	public function getPlugin(): CustomPvPBot
-	{
-		return CustomPvPBot::getInstance();
-	}
+    /**
+     * @return CustomPvPBot
+     */
+    public function getPlugin(): CustomPvPBot
+    {
+        return CustomPvPBot::getInstance();
+    }
 
-	/**
-	 * @return Player|null
-	 */
-	private function getAbusingPlayer(): ?Player
-	{
-		$players = $this->getPlugin()->getServer()->getOnlinePlayers();
-		if (count($players) > 0){
-			return $players[array_rand($players)];
-		}
-		return null;
-	}
+    /**
+     * @return Player|null
+     */
+    private function getAbusingPlayer(): ?Player
+    {
+        $players = $this->getPlugin()->getServer()->getOnlinePlayers();
+        if (count($players) > 0) {
+            return $players[array_rand($players)];
+        }
+        return null;
+    }
 
 }
